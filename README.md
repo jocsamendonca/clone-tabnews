b02cc7c90ac1636dd759f1a43d7edad6c6a2dc5d
Jocsã Mendonça <105027462+jocsamendonca@users.noreply.github.com>
2025-01-17 11:31:22 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implmentação do https://tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home (){
-    return <h1>Frase de Teste. 😎</h1>
+function Home() {
+  return <h1>Frase de Teste. 😎</h1>;
+}
+
+function teste1() {
+  console.log("teste de formatação");
 }
 
 export default Home;
